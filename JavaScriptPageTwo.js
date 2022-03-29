4a01a43d7584adc45be72ca9ce23adfca7aedd9a
Shaked sofer <54442082+shakedsofer@users.noreply.github.com>
2022-03-29 21:37:28 +0300

    Add files via upload

---
+++ JavaScriptPageTwo.js
@@ -0,0 +1,219 @@
+﻿let randomNumber = Math.floor(Math.random() * 21);
+
+alert(randomNumber);
+
+
+switch(randomNumber) {
+
+    
+    case 1:
+ 
+        alert("איזה תוספת שמים על הפיצה?");
+
+    break;
+    
+    case 2:
+  
+        alert("איך הצבים הפכו לנינג'ות ");
+    break;
+    
+    case 3:
+   
+        alert("אלו צבעים יש להם ?");
+    break;
+    
+    case 4:
+    alert("אילו נשקים יש להם ?");
+    break;
+    
+    case 5:
+    alert("איזה כלי נשק משתמש מיכאלנג׳לו?");
+    break;
+    
+    case 6:
+    alert("איך קוראים לבחורה בסדרה ?");
+    break;
+    
+    case 7:
+    alert("איפה גרים הצבים ?");
+    break;
+    
+    case 8:
+    alert("באיזה כלי רכב נוהגים בסדרה?");
+    break;
+    
+    case 9:
+    alert("מה שמו של מר קרב?");
+    break;
+    
+    case 10:
+    alert("איזה קול יש לגרי החילזון");
+    break;
+    
+    case 11:
+        alert("איפה מחביא מר קרב את כספו ?");
+    break;
+    
+    case 12:
+        alert("אישתו/חברה של פלנקטון היא..? ");
+    break;
+    
+    case 13:
+        alert("באיזה בית גר בובספוג?");
+    break;
+    
+    case 14:
+        alert("מאיזו ארץ מגיעה סנדי?");
+    break;
+    
+    case 15:
+        alert("איזה חיה פטריק");
+
+    break;
+    
+    case 16:
+        alert("באיזה בית גר פטריק");
+    break;
+    
+    case 17:
+        alert("איפה עודד עובד ?");
+    break;
+    
+    case 18:
+        alert("איך קוראים לשכנה?");
+    break;
+    
+    case 19:
+        alert(" מי כתב את שיר הפתיחה של הפג'מות?");
+    break;
+    
+    case 20:
+        alert("כמה דמויות ראשיות יש בסדרה פיג'מות?");
+    break;
+    
+    case 21:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 22:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 23:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 24:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 25:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 26:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 27:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 28:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 29:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 30:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 31:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 32:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 33:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 34:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 35:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 36:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 37:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 38:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 39:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 40:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 41:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 42:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 43:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 44:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 45:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 46:
+        alert("מה מוכרים אצל גרי?");
+    break;
+    
+    case 47:
+        alert("מה מוכרים אצל גרי?");
+    break;
+
+    case 48:
+        alert("מה מוכרים אצל גרי?");
+    break;
+
+    case 49:
+        alert("מה מוכרים אצל גרי?");
+    break;
+
+    case 50:
+        alert("מה מוכרים אצל גרי?");
+    break;
+     default:
+    
+}
+
+
+
+
